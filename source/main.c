9d6759842f76948fc25a9bbad630fde4c21a0107
simonmkwii <35590998+simonmkwii@users.noreply.github.com>
2018-06-24 04:24:08 +1000

    Small cleanup

--- source/main.c
+++ source/main.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <switch.h>
-const u64 SHARED_MEMORY_SIZE = 0x1100000;
+#define SHAREDMEMFONT_SIZE 0x1100000
+
 
 int main(int argc, char **argv)
 {
   Result rc;
   FILE *outFile;
   u8 *buffer, *sharedmem_addr;
-
+  
   gfxInitDefault();
   consoleInit(NULL);
   if (R_FAILED(rc = plInitialize())) {
@@ -19,20 +20,22 @@ int main(int argc, char **argv)
 
   sharedmem_addr = (u8*)plGetSharedmemAddr();
 
-  outFile = fopen ("shared_font.bin", "wb");
+  outFile = fopen("shared_font.bin", "wb");
   printf("\nDumping shared font...\n");
 
-  buffer = (u8*) calloc(SHARED_MEMORY_SIZE, sizeof(u8));
-  memcpy (buffer, sharedmem_addr, SHARED_MEMORY_SIZE);
-  if (fwrite(buffer, sizeof(u8), SHARED_MEMORY_SIZE, outFile) != SHARED_MEMORY_SIZE) {
+  buffer = (u8*) calloc(SHAREDMEMFONT_SIZE , sizeof(u8));
+  memcpy(buffer, sharedmem_addr, SHAREDMEMFONT_SIZE );
+  if (fwrite(buffer, sizeof(u8), SHAREDMEMFONT_SIZE , outFile) != SHAREDMEMFONT_SIZE) {
           printf("\nFailed to write shared font.\n");
-          fclose (outFile);
+          free(buffer);
+          fclose(outFile);
           gfxExit();
           plExit();
           return false;
   }
+  free(buffer);
   fclose(outFile);
-  
+
   printf("\nSuccessfully dumped to shared_font.bin!\n");
   printf("\n\n\nPress + to exit");
   int kDown;
