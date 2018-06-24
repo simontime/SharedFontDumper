089f2b46f1f87a1f1fdf5efc35ff031a13e4e4d9
BUP <39006831+yourmothergay@users.noreply.github.com>
2018-06-24 14:22:54 +0900

    indentation fixed

--- source/main.c
+++ source/main.c
@@ -5,50 +5,54 @@
 #define SHAREDMEMFONT_SIZE 0x1100000
 
 
-int main(int argc, char **argv)
-{
-  Result rc;
-  FILE *outFile;
-  u8 *buffer, *sharedmem_addr;
-  
-  gfxInitDefault();
-  consoleInit(NULL);
-  if (R_FAILED(rc = plInitialize())) {
-    printf("plInitialize() failed\n0x%x\n", rc);
-    return rc;
-  }
+int main(int argc, char **argv) {
+    Result rc;
+    FILE *outFile;
+    u8 *buffer, *sharedmem_addr;
+    int kDown;
 
-  sharedmem_addr = (u8*)plGetSharedmemAddr();
+    gfxInitDefault();
+    consoleInit(NULL);
 
-  outFile = fopen("shared_font.bin", "wb");
-  printf("\nDumping shared font...\n");
+    if (R_FAILED(rc = plInitialize())) {
+        printf("plInitialize() failed\n0x%x\n", rc);
+        return rc;
+    }
 
-  buffer = (u8*) calloc(SHAREDMEMFONT_SIZE , sizeof(u8));
-  memcpy(buffer, sharedmem_addr, SHAREDMEMFONT_SIZE );
-  if (fwrite(buffer, sizeof(u8), SHAREDMEMFONT_SIZE , outFile) != SHAREDMEMFONT_SIZE) {
-          printf("\nFailed to write shared font.\n");
-          free(buffer);
-          fclose(outFile);
-          gfxExit();
-          plExit();
-          return false;
-  }
-  free(buffer);
-  fclose(outFile);
+    sharedmem_addr = (u8*)plGetSharedmemAddr();
 
-  printf("\nSuccessfully dumped to shared_font.bin!\n");
-  printf("\n\n\nPress + to exit");
-  int kDown;
-  while(appletMainLoop()) {
-    hidScanInput();
-    kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-    if (kDown & KEY_PLUS) break;
-    gfxFlushBuffers();
-    gfxSwapBuffers();
-    gfxWaitForVsync();
-  }
-  printf("exiting...");
-  gfxExit();
-  plExit();
-  return 0;
+    outFile = fopen("shared_font.bin", "wb");
+    printf("Dumping shared font...\n");
+
+    buffer = (u8*)calloc(SHAREDMEMFONT_SIZE, sizeof(u8));
+
+    memcpy(buffer, sharedmem_addr, SHAREDMEMFONT_SIZE);
+    if (fwrite(buffer, sizeof(u8), SHAREDMEMFONT_SIZE, outFile) != SHAREDMEMFONT_SIZE) {
+        printf("Failed to write shared font.\n");
+        free(buffer);
+        fclose(outFile);
+        gfxExit();
+        plExit();
+        return 1;
+    }
+
+    free(buffer);
+    fclose(outFile);
+
+    printf("Successfully dumped to shared_font.bin!\n\n\n\n");
+    printf("Press + to exit");
+
+    while (appletMainLoop()) {
+        hidScanInput();
+        kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+        if (kDown & KEY_PLUS) break;
+
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+        gfxWaitForVsync();
+    }
+    
+    gfxExit();
+    plExit();
+    return 0;
 }
